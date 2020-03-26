22646f5eab61d7abca975aa6f3f37217529aab38
chandra1402 <chaandil1402>
2020-03-26 17:55:57 +0700

    upload download fix

--- application/controllers/api/File_materi.php
+++ application/controllers/api/File_materi.php
@@ -14,7 +14,7 @@ class File_materi extends REST_Controller
     private $arr_result = array();
     private $table                      = "file_materi";
     private $field_list = array('id_file_materi', 'file', 'jenis_file'
-            , 'id_materi','is_selesai','nama_file');
+            , 'id_materi','is_selesai','nama_file','id_pengguna');
     private $exception_field            = array();
     private $primary_key                = "id_file_materi";
     public function __construct()
@@ -59,60 +59,90 @@ class File_materi extends REST_Controller
 
         print json_encode($arr_result);
     }
+    public function find_file_materi_bysiswaid_post()
+    {
+        $idMateri= $this->input->post('id_materi');
+        $idPengguna= $this->input->post('id_pengguna');
 
+        $data_jadwal=$this->Mfile_materi->findBySiswaModelId($idMateri,$idPengguna, 'result');
+
+
+        if ($idMateri == "") {
+            $arr_result = array(
+                'prilude' => array(
+                    'status' => 'error',
+                    'message' => 'Coba Ulangi Lagi'
+                )
+            );
+        } else {
+            if (count($data_jadwal)==0) {
+                $arr_result = array(
+                 'prilude' => array(
+                     'status' => 'warning',
+                     'message' => 'Materi Tidak Ditemukan'
+                 )
+             );
+            } else {
+                $arr_result = array(
+                'prilude' => array(
+                    'status' => 'success',
+                    'message' => 'Data Pengguna Ditemukan.',
+                    'data_materi'     => $data_jadwal,
+                )
+            );
+            }
+        }
+
+        print json_encode($arr_result);
+    }
     public function upload_post()
     {
         $this->load->library('upload');
         $nama_file= $this->input->post('nama_file');
-        $file= $this->input->post('file');
+        $file= $this->input->post('fileNa');
+        $jenis_file= $this->input->post('jenis_file');
+        $id_materi= $this->input->post('id_materi');
+        $id_pengguna= $this->input->post('id_pengguna');
+        $config['upload_path'] = './file/';
+        $config['allowed_types'] = 'xlsx|xls|doc|docx|ppt|pptx|pdf';
 
-        $config['upload_path'] = '../file/';
-        $config['allowed_types']        = 'pdf';
-        $config['max_size']             = 100000;
-        $config['file_name'] = "masuk";
-
+        // load library upload
+        $this->load->library('upload', $config);
         $this->upload->initialize($config);
-         $certificateflag = $this->upload->do_upload("file");
-         if ($this->upload->do_upload("file"))
-         {
-            echo "New record created successfully";
+        if (!$this->upload->do_upload('fileNa')) {
+            $error = $this->upload->display_errors();
+            // menampilkan pesan error
+            print_r($error);
         } else {
-            echo "Gagal";
-        }
-
-        // $extension= $this->input->post('extension');
-        // header('Content-Type: bitmap; charset=utf-8');
-        //
-        // $fileUpload = '../file/';
-        //
-        // $ServerURL = 'http://192.168.18.9/nambahilmusukaresikapi/'.$fileUpload;
-        // echo $fileInfo = pathinfo($_FILES['ext']['nama_file']);
-        // $fileExtension = $fileInfo['extension'];
-
-        // $PdfFileURL = $this->GenerateFileNameUsingID() . '.' . $fileExtension;
-        // $PdfFileFinalPath = $fileUpload . $this->GenerateFileNameUsingID() . '.'. $fileExtension;
-        //
-        //
-        //  try{
-        //  move_uploaded_file($_FILES['ext']['tmp_name'],$PdfFileFinalPath);
-        //  // $InsertTableSQLQuery = "INSERT INTO materi (id_materi,file_link, nama_materi,id_guru,id_kelas,id_mata_pelajaran,id_pertemuan) VALUES (null,'$PdfFileURL', '$nama_materiNa','$id_guru','$id_kelas','$id_mata_pelajaran','$id_pertemuan') ";
-        //  echo "New record created successfully";
-        //  // mysqli_query($con,$InsertTableSQLQuery);
-        //  }catch(Exception $e){
-        //    echo "Gagal";
-        //  }
-         // mysqli_close($con);
-    }
-
-    public function GenerateFileNameUsingID()
-    {
-        $GenerateFileSQL = "SELECT max(id_file_materi) as id FROM file_materi";
-        $result=$this->db->query($GenerateFileSQL)->row()->id;
-
-        if ($result==null) {
-            return 1;
-        } else {
-            return ++$result;
+            $result = $this->upload->data();
+            $json2=json_encode($result);
+            $json = json_decode($json2, true);
+            $file_name = $json['file_name'];
+            $data = array(
+              'file' => $file_name,
+              'nama_file' => $nama_file,
+              'jenis_file' => $jenis_file,
+              'id_materi' => $id_materi,
+              'is_selesai' => '1',
+              'id_pengguna' => $id_pengguna,
+           );
+            $simpan=$this->Mfile_materi->create($data);
+            if ($simpan) {
+                $arr_result = array(
+               'prilude' => array(
+                   'status' => 'success',
+                   'message' => 'Upload Berhasil'
+               )
+           );
+            } else {
+                $arr_result = array(
+            'prilude' => array(
+                'status' => 'error',
+                'message' => 'Upload Gagal, silahkan coba lagi.'
+            )
+        );
+            }
+            print json_encode($arr_result);
         }
     }
 }
--- application/models/Mfile_materi.php
+++ application/models/Mfile_materi.php
@@ -1,10 +1,10 @@
 <?php
 class Mfile_materi extends CI_Model
 {
-  private $table                      = "file_materi";
+    private $table                      = "file_materi";
     private $table_materi                      = "materi";
     private $field_list = array('id_file_materi', 'file', 'jenis_file'
-          , 'id_materi','is_selesai','nama_file');
+          , 'id_materi','is_selesai','nama_file','id_pengguna');
     private $exception_field = array('');
     public function __construct()
     {
@@ -26,6 +26,31 @@ class Mfile_materi extends CI_Model
             return $this->db->get($this->table)->row();
         } else {
             return $this->db->get($this->table)->result();
+        }
+    }
+    public function findBySiswaModelId($id_materi,$idPengguna, $result_type)
+    {
+        $select = "";
+
+        $is_semester=$this->Mpengaturan->findByName('IS_SEMESTER')->nilai_pengaturan;
+
+        $this->db->select($this->table.".*");
+        $this->db->where($this->table . '.id_materi', $id_materi);
+        $this->db->where($this->table . '.id_pengguna', $idPengguna);
+
+
+        if ($result_type == "row") {
+            return $this->db->get($this->table)->row();
+        } else {
+            return $this->db->get($this->table)->result();
+        }
+    }
+    public function create($data)
+    {
+        if ($this->db->insert($this->table, $data)) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
